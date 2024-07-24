5e2013c22b521a592a601153cafbc988c6783218
Aditya Jha <adityajha0102@gmail.com>
2024-07-24 22:27:33 +0530

    Add files via upload

--- helpers/db_search.py
+++ helpers/db_search.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 
-def search_files(db, fs, query_fields):
+def search_files(db, fs, query_fields, check = 'specific'):
     try:
         # Construct the query dynamically
         query = {}
@@ -17,21 +17,10 @@ def search_files(db, fs, query_fields):
             # Convert ObjectId to string for JSON serialization
             document['_id'] = str(document['_id'])
             results.append(document)
-        print(len(results))
+        if check == 'top':
+            return results
         return jsonify(results)
 
     except Exception as e:
         return jsonify({'error': f"Error searching files in Database: {e}"}), 500
         
-'''if __name__=="__main__":
-    # Example search query (modify as per your requirement)
-    query_fields = {
-        "metadata.Simplified Type": "Fire",
-        # Add more fields as needed
-    }
-    results = search_files(query_fields)
-
-    # Process the results as needed
-    print(f"Found {len(results)} files matching the query.")
-
-    print(results)'''
